d085419bc64ba9ac0b6fb08bc4f775f3e368df36
olaulvas <ola.ulvaas@gmail.com>
2021-03-03 16:55:55 +0100

    Getters And Setters test passed for Product class

---
+++ src/main/java/se/lexicon/workshop_spring_boot/entity/Product.java
@@ -0,0 +1,80 @@
+package se.lexicon.workshop_spring_boot.entity;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Repository
+@Entity
+public class Product {
+    //Id, name, price
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(nullable = false, length = 50)
+    private String name;
+    @Column(nullable = false)
+    private Double price;
+
+    public Product() {
+    }
+
+    public Product(String name, Double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public Product(int id, String name, Double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return id == product.id && Objects.equals(name, product.name) && Objects.equals(price, product.price);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, price);
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price='" + price + '\'' +
+                '}';
+    }
+}
+
