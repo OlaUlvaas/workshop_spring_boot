a95b15a2d382bcb93ec1b554dcfff81768fc0fe8
olaulvas <ola.ulvaas@gmail.com>
2021-03-03 15:15:31 +0100

    Configure and create database mySQL

---
+++ src/main/java/se/lexicon/workshop_spring_boot/WorkshopSpringBootApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.workshop_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkshopSpringBootApplication.class, args);
+    }
+
+}
